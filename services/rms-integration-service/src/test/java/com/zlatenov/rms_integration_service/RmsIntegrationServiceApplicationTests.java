0437c8594743211bfcab7a88ecebfdd3e7bac5a0
Angel Zlatenov <angel.zlatenov@westernacher.com>
2024-12-04 00:16:48 +0200

    build: initialize rms-integration-service with Gradle and Spring Boot setup

---
+++ services/rms-integration-service/src/test/java/com/zlatenov/rms_integration_service/RmsIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlatenov.rms_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
